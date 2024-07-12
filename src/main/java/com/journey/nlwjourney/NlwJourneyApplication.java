37e255e18922d85bdb4451fe834735678ce56920
Lima-Developer <jvflima14@gmail.com>
2024-07-12 12:00:17 -0300

    feat: config files

---
+++ src/main/java/com/journey/nlwjourney/NlwJourneyApplication.java
@@ -0,0 +1,13 @@
+package com.journey.nlwjourney;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NlwJourneyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NlwJourneyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/journey/nlwjourney/NlwJourneyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.journey.nlwjourney;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NlwJourneyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
